bcb013e53cbe7c220a53367a941d483c5b2b7d6a
Alistair Scott <ali.scott@gmail.com>
2024-03-12 17:45:00 +0000

    Initial commit

---
+++ infra/dev/us1/ec2/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_instance" "web_app" {
+  ami           = "ami-674cbc1e"
+  instance_type = "t3.large"
+}
+
+module "ec2_instance" {
+  source  = "git@github.com:infracost/terraform-private-module-example22.git"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
